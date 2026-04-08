eee21c92767d9deebab8fa333f504a1da52b29a6
eduardasartorelli <eduarda.sartorelli123@gmail.com>
2026-04-08 11:38:48 -0300

    finalcorrigido

--- SilvinaHealthApp/App/_layout.js
+++ SilvinaHealthApp/App/_layout.js
@@ -1,28 +1,28 @@
-import React from 'react';
-import { stack } from 'expo-router';
-import { status } from 'expo-status-bar';
-
-export default function _layout() {
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+export default function Layout() {
   return (
     <>
-      <StatusBar style ='dark'/>
-      <Stack>
-        screenOpitions={{headerstyle: {
-            backgroudColor : '#f9f9f9',
-        },
-        headerTintColor: '#333',
-        headerTitleStyle: {
-            fontWeigth: 'bold',
-        }
+      <StatusBar style="dark" />
+      <Stack
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#f9f9f9',
+          },
+          headerTintColor: '#333',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
         }}
+      >
         <Stack.Screen
-         name="index"
-         options={{
+          name="index"
+          options={{
             title: 'Silvinha Health App',
             headerShown: true
-         }}
+          }}
         />
       </Stack>
     </>
-  )
+  );
 }
