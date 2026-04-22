1ab5dbfaab261e0b8b66a17a9313c2930d5312d9
eduardasartorelli <eduarda.sartorelli123@gmail.com>
2026-04-22 11:37:29 -0300

    final

--- SilvinaHealthApp/App/agua.js
+++ SilvinaHealthApp/App/agua.js
@@ -1,20 +1,3 @@
-{
-    resultado && (
-        <View style={styles.resultBox}>
-            <Text style={styles.resultLabel}>Recomendação diária:</Text>
-            <Text style={styles.resultValue}>{resultado.ml} ml</Text>
-            <Text style={styles.resultSecondary}>({resultado.l} litros)</Text>
-            <View style={styles.tipsContainer}>
-                <Text style={styles.tipsTitle}>Dicas:</Text>
-                <Text style={styles.tip}>• Beba um copo ao acordar</Text>
-                <Text style={styles.tip}>• Tenha uma garrafa sempre por
-                    perto</Text>
-                <Text style={styles.tip}>• Use aplicativos para lembrete</Text>
-            </View>
-        </View>
-    )
-}
-
 import React, { useState } from 'react';
 import {
     View,
